b2a1ac797ab136dcd2396531b23460ca234ddfff
Franck Tchouanga <125889035+TchouangaOcto@users.noreply.github.com>
2023-04-19 11:24:40 +0200

    added logs and loading from directory

--- cartoonize.py
+++ cartoonize.py
@@ -1,10 +1,29 @@
 # TO DO: import necessary libraries (Franck)
-import cv2
-import matplotlib.pyplot as plt
+#import cv2
+#import matplotlib.pyplot as plt
+import logging
+import os
+from pathlib import Path
 
-# TO DO :load data fully from the directory. (Adetutu))
+# Create and configure logger
+logging.basicConfig(filename="preprocess.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+# Creating an object
+logger = logging.getLogger()
+
+# Setting the threshold of logger to DEBUG
+logger.setLevel(logging.INFO)
+
+
+logger.info('Accessing the folder of images')
+try:
+    current_dir = os.getcwd()
+    data_dir = os.path.join(current_dir,'data/renamed_images')
+except Exception as e:
+    logger.error(e)
 
 # TO DO: choose the method randomly to cartoonize (Adetutu and Bryan)
 
-# TO DO:save images in a folder (Daniella)
+# TO DO:save images in a folder (Daniella/Adetutu)
 
