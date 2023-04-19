817bca9be03a7f7e858c5630519a7b1b33b8cb46
Franck Tchouanga <125889035+TchouangaOcto@users.noreply.github.com>
2023-04-19 12:38:57 +0200

    added the portion for loading and saving images into the transformed image folder

--- cartoonize.py
+++ cartoonize.py
@@ -15,6 +15,7 @@ logger = logging.getLogger()
 # Setting the threshold of logger to DEBUG
 logger.setLevel(logging.INFO)
 
+img = None #image default definition
 
 logger.info('Accessing the folder of images')
 try:
@@ -24,6 +25,20 @@ except Exception as e:
     logger.error(e)
 
 # TO DO: choose the method randomly to cartoonize (Adetutu and Bryan)
+logger.info('Cartoonization process on the images')
+try:
+    #Add your code here
+except Exception as e:
+    logger.error(e)
 
 # TO DO:save images in a folder (Daniella/Adetutu)
+logger.info('Creating the transform image folder and storing the images transformed')
+try:
+    filename = ""
+    data_dir = os.path.join(current_dir, 'transformed_images') # defining the transformes imafe folder
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+    cv2.imwrite(filename, img)
 
+except Exception as e:
+    logger.error(e)
